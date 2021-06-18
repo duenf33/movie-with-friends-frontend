0f6640e9be57e8fe48dcd212311d4851b6ba3150
fernando duenas <fernando39615@aim.com>
2021-06-18 15:40:40 -0400

    ommiting the https from the external links

--- src/component/Home.js
+++ src/component/Home.js
@@ -19,7 +19,7 @@ export class Home extends Component {
 							<p>
 								<NavLink
 									className="btn btn-secondary"
-									to="//https://calculator.fernandoduenas.com/">
+									to="//calculator.fernandoduenas.com/">
 									Go to page »
 								</NavLink>
 							</p>
@@ -32,7 +32,7 @@ export class Home extends Component {
 							<p>
 								<NavLink
 									className="btn btn-secondary"
-									to={"//https://mydolist.fernandoduenas.com"}>
+									to={"//mydolist.fernandoduenas.com"}>
 									Go to page »
 								</NavLink>
 							</p>
